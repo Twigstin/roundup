334619c0fc3272bfdaad84531b3bc921256971eb
Austin Aniobi <aniobi653@gmail.com>
2026-04-19 00:42:26 +0100

    feat: add roster route and page

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import { Routes, Route } from 'react-router-dom'
 import Dashboard from './pages/Dashboard'
 import NewTask from './pages/NewTask'
 import TaskDetail from './pages/TaskDetail'
+import Roster from './pages/Roster'
 
 function App() {
   return (
@@ -9,6 +10,7 @@ function App() {
       <Route path="/" element={<Dashboard />} />
       <Route path="/tasks/new" element={<NewTask />} />
       <Route path="/tasks/:id" element={<TaskDetail />} />
+      <Route path='/roster' element={<Roster />} />
     </Routes>
   )
 }
