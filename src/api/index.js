67e777cfbf9c3f9d6f1b863305276834c58576e9
Austin Aniobi <aniobi653@gmail.com>
2026-04-18 21:51:49 +0100

    fix: getEntries now filters by taskId

---
+++ src/api/index.js
@@ -0,0 +1,64 @@
+import { delay, readDB, writeDB } from './db.js';
+
+const TASKS_KEY = "roundup_tasks";
+const STUDENTS_KEY = "roundup_students";
+const ENTRIES_KEY = "roundup_entries";
+
+//create function to fetch tasks from database
+export const getTasks = async () => {
+    await delay(300)
+    return readDB(TASKS_KEY)
+}
+
+//create function to create and add new task to database
+export const createTask = async (task) => {
+    await delay(300)
+    //get list of tasks from database
+    const tasks = readDB(TASKS_KEY)
+    tasks.push(task)
+    //save update
+    writeDB(TASKS_KEY, tasks)
+    return task;
+}
+
+//create function to delete task
+export const deleteTask = async (taskId) => {
+    await delay(300)
+    const tasks = readDB(TASKS_KEY).filter(task => task.id !== taskId);
+    writeDB(TASKS_KEY, tasks)
+}
+
+//create function to fetch students from database
+export const getStudents = async () => {
+    await delay(300)
+    return readDB(STUDENTS_KEY)
+}
+
+//create function add new student to task
+export const createStudent = async (student) => {
+    await delay(300)
+    //fetch students
+    const students = readDB(STUDENTS_KEY)
+    students.push(student)
+    //save update
+    writeDB(STUDENTS_KEY, students);
+    return student
+}
+
+//create function to fetch entries from database
+export const getEntries = async (taskId) => {
+    await delay(300)
+    const entries = readDB(ENTRIES_KEY)
+    return entries.filter(e => e.taskId === taskId)
+}
+
+//create function to update entries
+export const updateEntry = async (entryId, updates) => {
+    await delay(300)
+    //fetch entries
+    const entries = readDB(ENTRIES_KEY)
+    const index = entries.findIndex(e => e.id === entryId)
+    entries[index] = { ...entries[index], ...updates }
+    writeDB(ENTRIES_KEY, entries)
+    return entries[index]
+}
